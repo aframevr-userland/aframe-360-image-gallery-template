20e1420afb43ba9f47ba4d70f04a77872f86b769
Christopher Van Wiemeersch <hearcomestreble@gmail.com>
2017-05-26 07:28:12 -0700

    THIS IS AFRAME-360-IMAGE-GALLERY-TEMPLATE

---
+++ app/js/initialize.js
@@ -0,0 +1,18 @@
+// For `animation` component.
+require('aframe-animation-component');
+
+// For `event-set` component.
+require('aframe-event-set-component');
+
+// For `layout` component.
+require('aframe-layout-component');
+
+// For template` and `template-set` components.
+require('aframe-template-component');
+
+// For `set-image` component.
+require('./components/set-image.js');
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Initialized page');
+});
